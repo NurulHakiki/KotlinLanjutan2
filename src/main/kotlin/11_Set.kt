7dd77ea6ce8b9e602db447d695104dcc3873618b
RPL-20 <nurulhakiki689@gmail.com>
2022-03-24 08:16:05 +0700

    Praktikum 1 Tentang array Set

---
+++ src/main/kotlin/11_Set.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //Set<T> menyimpan elemen unik; urutan mereka umumnya tidak terdefinisi.
+    val numbers = setOf(1, 2, 3, 4)
+    println("Number of elements: ${numbers.size}")
+    if (numbers.contains(1)) println("1 is in the set")
+    val numbersBackwards = setOf(4, 3, 2, 1)
+    println("The sets are equal: ${numbers == numbersBackwards}")
+    println(numbers.first() == numbersBackwards.first())
+    println(numbers.first() == numbersBackwards.last())
+    val strings = hashSetOf("a", "b", "c", "c")
+    println("My Set Values are"+strings)
+}
